3336b5959faa980a0a2bfc6c85f554180c317ccb
MARCO ANTONIO MERCADO <19121052@morelia.tecnm.mx>
2021-05-20 21:36:19 -0500

    Primer formato de preguntas... con respuesta

--- TAP2021_U1T2_Aplicación_Escolar/FormExamen.Designer.cs
+++ TAP2021_U1T2_Aplicación_Escolar/FormExamen.Designer.cs
@@ -29,20 +29,60 @@ namespace TAP2021_U1T2_Aplicación_Escolar
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelMateria = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // labelMateria
+            // 
+            this.labelMateria.AutoSize = true;
+            this.labelMateria.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMateria.Location = new System.Drawing.Point(168, 9);
+            this.labelMateria.Name = "labelMateria";
+            this.labelMateria.Size = new System.Drawing.Size(223, 24);
+            this.labelMateria.TabIndex = 0;
+            this.labelMateria.Text = "Nombre de la Materia: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1.- Pregunta: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 93);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(318, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "Respuestas...";
             // 
             // FormExamen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(621, 522);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelMateria);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormExamen";
             this.Text = "FormExamen";
             this.Load += new System.EventHandler(this.FormExamen_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelMateria;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
